596b96d3cbb7a270ff3954f9d97f4f4b91af7559
TomasGV1993 <tomas1720@hotmail.com>
2020-02-10 20:47:03 -0500

    Nuevo repositorio actualizaado

---
+++ EjercicioPractico1/DeclaracionVariables.js
@@ -0,0 +1,22 @@
+var Nombre = "Tomas"; // antigua manera de llamar las variables
+var Nombre = "Daniel";
+console.log(Nombre);
+/*---------------------------*/
+// let Nombre = "Tomas"; //modernizar JavaScript para no hacer malas practicas
+// let Nombre = "Daniel";
+// console.log(Nombre);
+
+let Costo = 5000;
+if (Costo > 3000) {
+    let Comida = "HotDog";
+}
+console.log("La comida: " + Comida);
+
+function Comer() {
+    var Cosa
+}
+
+for (let index = 0; index < 5; index++) {
+    
+}
+console.log(index);
